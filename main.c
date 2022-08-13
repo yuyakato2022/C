932012b63994cb07b749f5611e4f7910efab1e81
EC2 Default User <katoyuya.sevic1@outlook.jp>
2022-08-13 08:39:34 +0000

    1day practice

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+  printf("123¥n456¥n789¥n");
+  printf("シングルクオーテーション : ¥’ダブルクオーテーション : ¥”¥n");
+  printf("¥t円マーク¥¥¥n");
+  
+  printf("こんにちわ。私の名前は%sです。¥n年齢は%d歳です。¥n", "山田太郎", 20);
+  printf("イニシャルは、%cです。¥n", 'Y');
+  printf("%f + %f = %f¥n", 1.2,2.7, 1.2 + 2.7);
+  
+  printf("ABC");
+  printf("DEF¥n");
+}
