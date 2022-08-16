7ee317c656fbcea17cc0b872ea386c788d600107
EC2 Default User <katoyuya.sevic1@outlook.jp>
2022-08-16 05:49:49 +0000

    3day practice

--- main3.c
+++ main3.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 void main(){
-  // int num;
-  // printf("1-3の値を入力してください:");
-  // scanf("%d", &num);
-  // if(num == 1){
-  //   printf("one¥n");
-  // }else if(num == 2){
-  //   printf("two¥n");
-  // }else{
-  //   printf("不適切な値です。¥n");
+  int num;
+  printf("1-3の値を入力してください:");
+  scanf("%d", &num);
+  if(num == 1){
+    printf("one¥n");
+  }else if(num == 2){
+    printf("two¥n");
+  }else{
+    printf("不適切な値です。¥n");
   
-  // int dice;
-  // printf("1から6の数値を入力してください:");
-  // scanf("%d", &dice);
+  int dice;
+  printf("1から6の数値を入力してください:");
+  scanf("%d", &dice);
   
-  // if(1 <= dice && dice <= 6){
-  //   if(dice == 2 || dice == 4 || dice ==6){
-  //     printf("丁です。¥n");
-  //   }else{
-  //     printf("半です。¥n");
-  //   }
-  // }else{
-  //   printf("範囲外です¥n");
-  // }
+  if(1 <= dice && dice <= 6){
+    if(dice == 2 || dice == 4 || dice ==6){
+      printf("丁です。¥n");
+    }else{
+      printf("半です。¥n");
+    }
+  }else{
+    printf("範囲外です¥n");
+  }
  
  int num;
  printf("1-3の値を入力してください");
