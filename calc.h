cbf431cdd62fb0951d08aa767fd9163b6bec32aa
EC2 Default User <katoyuya.sevic1@outlook.jp>
2022-08-20 12:50:15 +0000

    7day practice

---
+++ calc.h
@@ -0,0 +1,6 @@
+#ifndef _CALC_H_
+#define _CALC_H_
+ 
+double avg(double,double);
+ 
+#endif // _CALC_H_
---
+++ main7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "calc.h"
+  
+void main(){
+   double d1,d2,d3;
+   double a = 1.2,b = 3.4,c = 2.7;
+   
+   d1 = avg(a,b);
+   d2 = avg(4.1,5.7);
+   d3 = avg(c,2.8);
+   printf("d1 = %f,d2 = %f,d3 = %f¥n",d1,d2,d3);
+}
